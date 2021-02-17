7ee49a3c8de5a96353c3ccc3ba323122548b0f0b
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-17 12:08:06 -0300

    Yelp MVP Exercise - Creating Models with Validations, View, Controllers + Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Iniciando...'
+5.times do
+  faker_name = Faker::Restaurant.name
+  faker_address = Faker::Address.street_name
+  faker_phone_number = Faker::PhoneNumber.cell_phone_in_e164
+  category = %w[chinese italian japanese french belgian].sample
+  # Cria um restaurante
+  Restaurant.create(name: faker_name, address: faker_address, phone_number: faker_phone_number, category: category)
+end
+puts 'Finalizando...'
