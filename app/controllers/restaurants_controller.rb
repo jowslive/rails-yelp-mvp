c2d0226fb4d972f0f88be92748a597bf0e918859
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-17 14:44:17 -0300

    Yelp MVP Exercise Complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,34 @@
-class RestaurantController < ApplicationController
+class RestaurantsController < ApplicationController
+  before_action :find, only: %i[show edit]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show; end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  # before show, edit run this code!
+  def find
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
